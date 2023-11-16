bca2698d01c217e10f3b6409a9a25fc2d6f9d442
Wylunaa <wylunaa@gmail.com>
2023-11-16 14:36:42 -0400

    style commit

--- doheart.js
+++
@@ -1,8 +0,0 @@
-const heart = document.getElementById('heart');
-
-document.addEventListener('keydown', function(event){
-   if(event.code === 'space' || event.code === 'Enter'){
-    heart.classList.toggle('pulse');
-   } 
-});
-    
